ee8b19925aeab84751d7decdaa404942abffce18
RompkoH <helio.rompkovski@landisgyr.com>
2024-05-07 14:18:00 -0300

    add Appbar on layout

--- app/src/main/java/com/example/firstaluraclass/mainActivity.java
+++ app/src/main/java/com/example/firstaluraclass/mainActivity.java
@@ -2,17 +2,13 @@ package com.example.firstaluraclass;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class mainActivity extends Activity {
@@ -21,6 +17,20 @@ public class mainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>();
+
+        alunos.add("Joao");
+        alunos.add("Matheus");
+        alunos.add("Pabulo");
+
+        ListView ListaAlunos = findViewById(R.id.listAlunos);
+
+        ListaAlunos.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
 
     }
 }
